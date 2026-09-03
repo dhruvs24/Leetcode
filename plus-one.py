c1a23bebf6e768722a4aecab95d1615ecb56e3da
Dhruv Singh <singhdh2@msu.edu>
2026-09-02 21:54:31 -0400

    my solution

--- plus-one.py
+++ plus-one.py
@@ -1,2 +1,16 @@
 class Solution(object):
-    
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits = digits[::-1]
+        for i in range(len(digits)):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits[::-1]
+        
+        digits.append(1)
+        return digits[::-1]
