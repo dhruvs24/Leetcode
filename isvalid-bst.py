f15310666a73688e4d29559a6d26edef5a9846df
Dhruv Singh <thesinghdhruv.29@gmail.com>
2026-09-14 23:05:21 -0400

    sol

---
+++ isvalid-bst.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def isValid(node, left, right):
+            if node is None:
+                return True
+            if left >= node.val:
+                return False
+            if right <= node.val:
+                return False
+            return isValid(node.left, left, node.val) and isValid(node.right, node.val, right)
+        return isValid(root, float("-inf"), float("inf"))
