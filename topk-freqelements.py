6119dd0769cd7a7fdd0282d8fc8319e97fb5c8af
Dhruv Singh <thesinghdhruv.29@gmail.com>
2026-09-25 23:43:21 -0400

    optimal solution comment

--- topk-freqelements.py
+++ topk-freqelements.py
@@ -18,3 +18,21 @@ class Solution:
             result_array.append(sorted_array[i][0])
         
         return result_array
+
+# optimal solution- bucket sort O(n) time
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         count = {}
+#         freq = [[] for i in range(len(nums) + 1)]
+
+#         for num in nums:
+#             count[num] = 1 + count.get(num, 0)
+#         for num, cnt in count.items():
+#             freq[cnt].append(num)
+
+#         res = []
+#         for i in range(len(freq) - 1, 0, -1):
+#             for num in freq[i]:
+#                 res.append(num)
+#                 if len(res) == k:
+#                     return res
